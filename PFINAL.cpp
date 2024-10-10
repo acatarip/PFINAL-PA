7db7b49bccbe655b9595136071b4ec732d8207ba
Achatac <achatac@unjbg.edu.pe>
2024-10-10 09:43:46 -0500

    Algunos cambios realizados

--- PFINAL.cpp
+++ PFINAL.cpp
@@ -6,17 +6,59 @@ struct DATOS{
 	string nombre;
 };
 
+DATOS DEPO[100];  //arreglo de datos principal
+int m = 0;  //contador de pacientes totales registrados
+
 void agregar(DATOS A[], int &m, int b);
 void mostrarPorEdades (DATOS A[], int m);
 void mostrarUsuarios (DATOS A[], int m);
 void buscarUsuario (DATOS A[], int m);
 void eliminarUsuarios (DATOS A[], int &m);
 
-DATOS DEPO[100];  //arreglo de datos principal
-int m = 0;  //contador de pacientes totales registrados
+
+void agregar(DATOS A[], int &m, int b){
+	for (int i = m; i < (b + m); i++){  //desde m hasta b+m para que no se sobrescriban los datos
+		cout<<"\nIngrese nombre del usuario "<<i+1<<": ";
+		cin>>A[i].nombre;
+		cout<<"Ingrese edad del usuario "<<i+1<<": ";
+		cin>>A[i].edad;
+	}
+	m = m + b;
+}
+
+void mostrarPorEdades (DATOS A[], int m){
+	
+}
+
+void mostrarUsuarios (DATOS A[], int m){
+	cout<<"Lista de usuarios registrados:\n";
+	for (int i=0; i<m; i++){
+		cout<<i+1<<". Nombre: "<<DEPO[i].nombre;
+		cout<<"\n   Edad: "<<DEPO[i].edad<<" anios\n\n";
+	}
+	system("pause");
+}
+
+void buscarUsuario (DATOS A[], int m){
+	system("cls");
+	int buscar;
+	cout<<"        BUSCAR USUARIO\n";
+	cout<<"1. Buscar por nombre\n";
+	cout<<"2. Buscar por edad\n";
+	cout<<"0. Salir\n\n";
+	cout<<"Ingrese su opcion: ";
+	cin>>buscar;
+	
+}
+
+void eliminarUsuarios (DATOS A[], int &m){
+	system("cls");
+	cout<<"        ELIMINAR USUARIO\n";
+	
+}
 
 int main(){
-	int n;
+	int opcion;
 	do{
 		system("cls");
 		cout<<"        MENU\n";
@@ -26,9 +68,9 @@ int main(){
 		cout<<"4. Eliminar usuario\n";
 		cout<<"0. Salir\n\n";
 		cout<<"Ingrese su opcion: ";
-		cin>>n;
+		cin>>opcion;
 		
-		switch (n){
+		switch (opcion){
 			case 1:
 				int cant;
 				system("cls");
@@ -71,51 +113,9 @@ int main(){
 				system("pause");
 			break;
 		}
-	} while(n!=0);
+	} while(opcion!=0);
 	
 	
 	
 	return 0;
-}
-
-
-void agregar(DATOS A[], int &m, int b){
-	for (int i=m; i<(b+m); i++){  //desde m hasta b+m para que no se sobrescriban los datos
-		cout<<"\nIngrese nombre del usuario "<<i+1<<": ";
-		cin>>A[i].nombre;
-		cout<<"Ingrese edad del usuario "<<i+1<<": ";
-		cin>>A[i].edad;
-	}
-	m = m + b;
-}
-
-void mostrarPorEdades (DATOS A[], int m){
-	
-}
-
-void mostrarUsuarios (DATOS A[], int m){
-	cout<<"Lista de usuarios registrados:\n";
-	for (int i=0; i<m; i++){
-		cout<<i+1<<". Nombre: "<<DEPO[i].nombre;
-		cout<<"\n   Edad: "<<DEPO[i].edad<<" anios\n\n";
-	}
-	system("pause");
-}
-
-void buscarUsuario (DATOS A[], int m){
-	system("cls");
-	int buscar;
-	cout<<"        BUSCAR USUARIO\n";
-	cout<<"1. Buscar por nombre\n";
-	cout<<"2. Buscar por edad\n";
-	cout<<"0. Salir\n\n";
-	cout<<"Ingrese su opcion: ";
-	cin>>buscar;
-	
-}
-
-void eliminarUsuarios (DATOS A[], int &m){
-	system("cls");
-	cout<<"        ELIMINAR USUARIO\n";
-	
 }
