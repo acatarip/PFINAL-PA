3c72f740e3bb75fedb9c023c0481603fd882b7ea
Achatac <achatac@unjbg.edu.pe>
2024-10-10 16:07:30 -0500

    aun mas cambios

--- PFINAL.cpp
+++ PFINAL.cpp
@@ -2,45 +2,39 @@
 #include <string>
 using namespace std;
 
-struct DATOS{
-	int edad;
+struct INFORMACION{
 	string nombre;
+	string descripcion;
+	float precio;
+	int cantidad;
 };
 
-DATOS DEPO[100];  //arreglo de datos principal
+INFORMACION producto[100];  //arreglo de datos principal
 int m = 0;  //contador de pacientes totales registrados
 
-void agregar(DATOS A[], int &m, int b);
-void mostrarPorEdades (DATOS A[], int m);
-void mostrarUsuarios (DATOS A[], int m);
-void buscarUsuario (DATOS A[], int m);
-void eliminarUsuarios (DATOS A[], int &m);
+void agregarProducto(INFORMACION producto[], int &m, int b);
+void mostrarProducto (INFORMACION producto[], int m);
+void buscarProducto (INFORMACION producto[], int m);
+void eliminarProducto (INFORMACION producto[], int &m);
 
 
-void agregar(DATOS A[], int &m, int b){
-	for (int i = m; i < (b + m); i++){  //desde m hasta b+m para que no se sobrescriban los datos
-		cout<<"\nIngrese nombre del usuario "<<i+1<<": ";
-		cin>>A[i].nombre;
-		cout<<"Ingrese edad del usuario "<<i+1<<": ";
-		cin>>A[i].edad;
-	}
-	m = m + b;
+void agregarProducto(INFORMACION producto[], int &m, int b)
+{
+
 }
 
-void mostrarPorEdades (DATOS A[], int m){
-	
+void mostrarProducto (INFORMACION producto[], int m)
+{
+
 }
 
-void mostrarUsuarios (DATOS A[], int m){
-	cout<<"Lista de usuarios registrados:\n";
-	for (int i=0; i<m; i++){
-		cout<<i+1<<". Nombre: "<<DEPO[i].nombre;
-		cout<<"\n   Edad: "<<DEPO[i].edad<<" anios\n\n";
-	}
-	system("pause");
+void buscarProducto (INFORMACION producto[], int m)
+{
+
 }
 
-void buscarUsuario (DATOS A[], int m){
+
+void buscarProducto (INFORMACION producto[], int m){
 	system("cls");
 	int buscar;
 	cout<<"        BUSCAR USUARIO\n";
@@ -52,18 +46,16 @@ void buscarUsuario (DATOS A[], int m){
 	
 }
 
-void eliminarUsuarios (DATOS A[], int &m){
-	system("cls");
-	cout<<"        ELIMINAR USUARIO\n";
+void eliminarProducto (INFORMACION producto[], int &m){
 	
 }
 
 int main(){
 	int opcion;
-	do{
+	do {
 		system("cls");
 		cout << "=====================================================" << endl;
-		cout <<"                         MENU" << endl;
+		cout << "                        MENU" << endl;
 		cout << "=====================================================" << endl;
 		cout << " 1. Agregar usuario" << endl;
 		cout << " 2. Usuarios registrados" << endl;
@@ -76,50 +68,24 @@ int main(){
 		
 		switch (opcion){
 			case 1:
-				int cant;
-				system("cls");
-				cout<<"        AGREGAR USUARIO\n";
-				cout<<"Ingrese cantidad de usuarios: "; 
-				cin>>cant;
-				agregar(DEPO, m, cant);
+				
 			break;
 			case 2:
-				int op;
-				system("cls");
-				cout<<"        USUARIOS REGISTRADOS\n";
-				cout<<"1. Grupos por edades\n";
-				cout<<"2. Todos los usuarios\n";
-				cout<<"0. Regresar al menu\n\n";
-				cout<<"Ingrese opcion: ";
-				cin>>op;
-				switch (op){
-					case 1:
-						mostrarPorEdades (DEPO, m);
-					break;
-					case 2:
-						mostrarUsuarios (DEPO, m);
-					break;
-					case 0:
-						cout<<"Regresando al menu...";
-					break;
-				}
+
 			break;
 			case 3:
-				buscarUsuario (DEPO, m);
+
 			case 4:
-				eliminarUsuarios (DEPO, m);
+
 			break;
 			case 0:
-				cout<<"\nSaliendo . . .\n";
+				cout << "Saliendo . . ." << endl;
 			break;
 			default:
-				cout<<"\nOpcion no valida. Intentelo de nuevo.\n";
+				cout << "Opcion no valida. Intentelo de nuevo." << endl;
 				system("pause");
 			break;
 		}
-	} while(opcion!=0);
-	
-	
-	
+	} while(opcion != 0);
 	return 0;
 }
