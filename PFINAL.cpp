44b1ee5d260a53c87243a51ef6475a64be646cf0
acatarip <acatarip@unjbg.edu.pe>
2024-10-10 17:15:36 -0500

    Actualizar funcion agregar

--- PFINAL.cpp
+++ PFINAL.cpp
@@ -9,23 +9,77 @@ struct INFORMACION{
 	int cantidad;
 };
 
-INFORMACION producto[100];  //arreglo de datos principal
-int m = 0;  //contador de pacientes totales registrados
+INFORMACION producto[100];
+int Indice = 0;
 
-void agregarProducto(INFORMACION producto[], int &m, int b);
-void mostrarProducto (INFORMACION producto[], int m);
+void ordenacion();
+void agregarProducto(INFORMACION producto[], int &Indice);
+void mostrarProducto (INFORMACION producto[], int Indice);
 void buscarProducto (INFORMACION producto[], int m);
 void eliminarProducto (INFORMACION producto[], int &m);
 
-
-void agregarProducto(INFORMACION producto[], int &m, int b)
+void agregarProducto(INFORMACION producto[], int &Indice)
 {
-
+	int cant;
+	system("cls");
+	cout << "        AGREGAR PRODUCTO\n";
+	cout << "Cuantos productos (diferentes) desea agregar: ";
+	cin >> cant;
+	
+	for (int i=0; i<cant; i++){
+		string nombreTemp;
+		system("cls");
+		cout << "        AGREGAR PRODUCTO\n";
+		cout<<"Ingrese nombre del producto "<< i+1 <<": ";
+		cin.ignore();
+		getline(cin, nombreTemp);  //AGREGA getline PARA OVBIAR ESPACIADO !!!!!!!!!!!!
+		
+		bool encontrado = false;
+		int cantidadTemp;
+		
+		//Funcion para verificar si se REPITE el NOMBRE del producto
+		for (int j=0; j<Indice; j++){
+			if (nombreTemp == producto[j].nombre){
+				encontrado = true;
+				cout<<"Ingrese cantidad: ";  //Si se repite, solo ingresa cantidad.
+				cin>> cantidadTemp;
+				producto[i].cantidad = producto[i].cantidad + cantidadTemp;
+			}
+		}
+		
+		if (!encontrado){
+			Indice = Indice + 1;
+			cout<<"Ingrese una descripcion: ";
+			cin.ignore();
+			getline(cin, producto[i].descripcion);
+			
+			cout<<"Ingrese un precio: ";
+			cin>>producto[i].precio;
+			
+			cout<<"Ingrese cantidad: ";
+			cin>> cantidadTemp;
+			producto[i].cantidad = producto[i].cantidad + cantidadTemp;
+		}
+		
+		
+		
+	}
+	
+	
+	
+	ordenacion();
 }
 
-void mostrarProducto (INFORMACION producto[], int m)
+void mostrarProducto (INFORMACION producto[], int Indice)
 {
-
+	system("cls");
+	cout<<"         MOSTRAR PRODUCTO\n";
+	if (Indice == 0){
+		cout<<"\nAun no se registraron productos.\n";
+		system("pause");
+		return;
+	}
+	
 }
 
 void buscarProducto (INFORMACION producto[], int m)
@@ -57,8 +111,8 @@ int main(){
 		cout << "=====================================================" << endl;
 		cout << "                        MENU" << endl;
 		cout << "=====================================================" << endl;
-		cout << " 1. Agregar usuario" << endl;
-		cout << " 2. Usuarios registrados" << endl;
+		cout << " 1. Agregar producto" << endl;
+		cout << " 2. Mostrar productos" << endl;
 		cout << " 3. Buscar usuario" << endl;
 		cout << " 4. Eliminar usuario" << endl;
 		cout << " 0. Salir" << endl;
@@ -68,9 +122,10 @@ int main(){
 		
 		switch (opcion){
 			case 1:
-				
+				agregarProducto(producto, Indice);
 			break;
 			case 2:
+				mostrarProducto (producto, Indice);
 
 			break;
 			case 3:
